2608a11348fdaedc2c5d565fbea26ed54196170c
syxanash <si.marzulli@gmail.com>
2026-03-07 18:18:18 +0000

    fix config value by default

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
   BACKEND_URL: import.meta.env.VITE_BACKEND_URL,
   WEBSOCKET_URL: import.meta.env.VITE_WEBSOCKET_URL,
   OLLAMA_URL: import.meta.env.VITE_OLLAMA_URL,
-  FULLSCREEN: false,
+  FULLSCREEN: true,
 };
 
 export default config;
